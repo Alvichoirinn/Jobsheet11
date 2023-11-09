84a302c851842c23dae0ae7e68ad276d95efaab0
alvichoirinn <avialvi08@gmail.com>
2023-11-09 15:37:33 +0700

    Jobsheet11

--- NestedLoopNIM.java
+++ NestedLoopNIM.java
@@ -1,12 +1,12 @@
 import java.util.Scanner;
-public class NestedLoop_2341760191 {
+public class NestedLoopNIM {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         double [][] temps = new double [5][7];
 
     for(int i = 0; i < temps.length; i++){
         System.out.println("Kota ke-" + i);
-    for(int j = 0; j < temps[0].length; j++){
+        for(int j = 0; j < temps[0].length; j++){
         System.out.print("Hari ke-" + (j+1)+ ":");
         temps[i][j]= sc.nextDouble();
     }
