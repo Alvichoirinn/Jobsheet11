1b6cbe0491415a40a53ba406c1b538241b134a7b
alvichoirinn <avialvi08@gmail.com>
2023-11-13 07:55:52 +0700

    Jobsheet11

--- TugasIndividu3.java
+++ TugasIndividu3.java
@@ -13,7 +13,7 @@ public class TugasIndividu3 {
             for (int i = 1; i <= N; i++) {
                 for (int j = 1; j <= N; j++) {
                     if (i == 1 || i == N || j == 1 || j == N) {
-                        System.out.print("5"); 
+                        System.out.print("" + N); 
                     } else {
                         System.out.print(" ");
 
